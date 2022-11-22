39331ae244eed9e5bd69cd8e7ed08c8a3eba8221
weston-bailey <wdbailey87@gmail.com>
2022-11-22 11:22:24 -0800

    debug

--- app.js
+++ app.js
@@ -7,32 +7,52 @@ console.log('script is linked!')
 
 // don't run this until the dom content has loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // window.fetch() -- retrieves data from our data API
-    fetch('https://swapi.dev/api/films/') // returns a Promise (async operation)
-        // dot then allows us to wait for the async operation to complete
-        .then(responseData => {
-            // this cb runs when the server response
-            // converts raw response into usable json data (this is a fetch thing)
-            return responseData.json() //returning from a dot then, lets us dot then again
-        })
-        .then(swapiJson => {
-            // actual useful swapi data
-            console.log(swapiJson.results)
-            // select the body
-            const body = document.querySelector('body')
-            // loop over each movie and append the title and opening crawl to the page
-            swapiJson.results.forEach(result => {
-                // create some elements to load on the body
-                const div = document.createElement('div')
-                const p = document.createElement('p')
-                const h2 = document.createElement('h2')
-                // modify the properties of the elements
-                p.innerText = result.opening_crawl
-                h2.innerText = result.title
-                console.log(p, h2)
-                // append new elements to the dom
-                div.append(h2, p)
-                body.append(div)
+    // // window.fetch() -- retrieves data from our data API
+    // fetch('https://swapi.dev/api/films/') // returns a Promise (async operation)
+    //     // dot then allows us to wait for the async operation to complete
+    //     .then(responseData => {
+    //         // this cb runs when the server response
+    //         // converts raw response into usable json data (this is a fetch thing)
+    //         return responseData.json() //returning from a dot then, lets us dot then again
+    //     })
+    //     .then(swapiJson => {
+    //         // actual useful swapi data
+    //         console.log(swapiJson.results)
+    //         // select the body
+    //         const body = document.querySelector('body')
+    //         // loop over each movie and append the title and opening crawl to the page
+    //         swapiJson.results.forEach(result => {
+    //             // create some elements to load on the body
+    //             const div = document.createElement('div')
+    //             const p = document.createElement('p')
+    //             const h2 = document.createElement('h2')
+    //             // modify the properties of the elements
+    //             p.innerText = result.opening_crawl
+    //             h2.innerText = result.title
+    //             console.log(p, h2)
+    //             // append new elements to the dom
+    //             div.append(h2, p)
+    //             body.append(div)
+    //         })
+    //     })
+    // ? starts the query strings, which are key/value pairs in a url
+    // https://www.google.com/search?q=mango (query string google search example)
+    const requestUrl = 'https://randomuser.me/api/?results=10'
+    fetch(requestUrl)
+        .then(responseData => responseData.json()) // implicit return arrow function
+        .then(peopleData => {
+            console.log(peopleData)
+            const peopleList = document.querySelector('#peopleList')
+            console.log(peopleList)
+            peopleData.results.forEach(person => {
+                console.log(person.name.title, person.name.first, person.name.last)
+                // create the new element
+                const li = document.createElement('li')
+                // set the element propeties
+                li.innerText = `${person.name.title} ${person.name.first} ${person.name.last}`
+                // append the element to the dom
+                peopleList.append(li)
             })
         })
+        .catch(console.warn) // catch handles any errors that might occur in the fetch process
 })
